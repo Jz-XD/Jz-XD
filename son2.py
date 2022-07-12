c2417ddba8537c91f0fcf993798b08a0915f2939
Jazer Kus Adhitya <109030790+Jz-XD@users.noreply.github.com>
2022-07-12 15:36:39 +0700

    Update son2.py

--- son2.py
+++ son2.py
@@ -59,18 +59,18 @@ for xd in range(10000):
 	ugen2.append(uaku)
 
 
-	aa='Mozilla/5.0 (X11'
+	aa='Mozilla/5.0 (Linux; Android 10'
 	b=random.choice(['6','7','8','9','10','11','12'])
-	c='CrOS armv7l 4537.56.0'
+	c='motorola edge plus'
 	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
 	e=random.randrange(1, 999)
 	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-	g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.38'
+	g='AppleWebKit/537.36 (KHTML, seperti Gecko) Chrome/'
 	h=random.randrange(73,100)
 	i='0'
 	j=random.randrange(4200,4900)
 	k=random.randrange(40,150)
-	l='Safari/537.36'
+	l='Mobile Safari/537.36'
 	uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
 	ugen.append(uaku2)
 for x in range(10):
